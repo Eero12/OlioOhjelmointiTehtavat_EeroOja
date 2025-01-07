efba98c32734adb2250bf886430ed0db79d7f9c6
Eero12 <eerooja18@gmail.com>
2025-01-07 11:44:54 +0200

    arvauspeli toimii

---
+++ viikko1/Arvauspeli/arvauspeli/arvauspeli.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int vastaus = -1;
+    srand(time(NULL));
+    int rndnumber = rand() % 20;
+    //cout << rndnumber << endl;
+    while (vastaus != rndnumber) {
+    cin >> vastaus;
+        if(vastaus < rndnumber){
+        cout << "Numero on suurempi " << endl;
+        }
+        else if(vastaus > rndnumber){
+            cout << "Numero on pienempi " << endl;
+        }
+    }
+    cout << "OIKEIN!" << endl;
+    return 0;
+}
