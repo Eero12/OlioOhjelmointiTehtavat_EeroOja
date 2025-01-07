f6a91f882df1fd8d4e2d5e535800b48217f3fedb
Eero12 <eerooja18@gmail.com>
2025-01-07 11:24:59 +0200

    testailuja

--- viikko1/EkaOhjelma/ekaohjelma.cpp
+++ viikko1/EkaOhjelma/ekaohjelma.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int k = 34;
+    float a = 4.1;
+    double b = 1.5;
+    char merkkijono[] = "qwertyuiopasdfghjklzxcvbnm";
+    string stringit = "";
+
+    cin >> stringit;
+    cout << "Hello World!" << " " << b << " "  << k << " " << a << " "  << merkkijono << " "  << stringit.length() <<endl;
+    for (int var = 0; var < stringit.length() +1; ++var) {
+        cout << merkkijono[var] <<endl;
+    }
     return 0;
 }
