e226ff385dc0716ea4737cb3b8e269986cdd33da
Eero12 <eerooja18@gmail.com>
2025-01-08 12:43:42 +0200

    valmis

--- viikko1/Arvauspeli/arvauspeli/arvauspeli.cpp
+++ viikko1/Arvauspeli/arvauspeli/arvauspeli.cpp
@@ -4,21 +4,29 @@
 
 using namespace std;
 
+int game(int maxnum){
+int vastaus = -1;
+    int arvaukset = 0;
+srand(time(NULL));
+int rndnumber = rand() % maxnum;
+//cout << rndnumber << endl;
+while (vastaus != rndnumber) {
+    cin >> vastaus;
+    if(vastaus < rndnumber){
+        arvaukset++;
+        cout << "Numero on suurempi " << endl;
+    }
+    else if(vastaus > rndnumber){
+        cout << "Numero on pienempi" << endl;
+        arvaukset++;
+    }
+}
+
+cout << "OIKEIN!" <<" Arvausten maara: " << arvaukset << endl;
+return 0;
+}
+
 int main()
 {
-    int vastaus = -1;
-    srand(time(NULL));
-    int rndnumber = rand() % 20;
-    //cout << rndnumber << endl;
-    while (vastaus != rndnumber) {
-    cin >> vastaus;
-        if(vastaus < rndnumber){
-        cout << "Numero on suurempi " << endl;
-        }
-        else if(vastaus > rndnumber){
-            cout << "Numero on pienempi" << endl;
-        }
-    }
-    cout << "OIKEIN!" << endl;
-    return 0;
+    game(10);
 }
