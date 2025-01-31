83efee5a80c3b55b75723c74cc12a9ab30608c89
Eero12 <eerooja18@gmail.com>
2025-01-31 09:02:19 +0200

    eka

---
+++ viikko4/Viikkotehtava4/pankkitili.h
@@ -0,0 +1,25 @@
+#ifndef PANKKITILI_H
+#define PANKKITILI_H
+
+using namespace std;
+
+
+class Pankkitili
+{
+public:
+    Pankkitili();
+
+
+
+private:
+
+
+
+protected:
+    string omistaja;
+
+
+
+};
+
+#endif // PANKKITILI_H
